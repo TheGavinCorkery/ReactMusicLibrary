4e852753a215c5ffbefb4e2519888cc062527d40
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-07 12:07:13 -0400

    Added SongForm and MusicTable components

---
+++ src/components/MusicTable/MusicTable.jsx
@@ -0,0 +1,18 @@
+import './MusicTable.css'
+import React, { Component } from 'react';
+
+class MusicTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="row">
+
+            </div>
+         );
+    }
+}
+ 
+export default MusicTable;
