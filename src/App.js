ccd1dd03fa8f29d5e294e42be4b476929a555b58
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-07 14:41:55 -0400

    Added table of songs and button to delete song in row

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MusicTable from './components/MusicTable/MusicTable';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="container">
-      
+      <MusicTable />
     </div>
   );
 }
--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -1,15 +1,64 @@
 import './MusicTable.css'
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class MusicTable extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            songs: []
+         }
     }
+
+    componentDidMount() {
+        this.getAllSongs()
+    }
+
+    async getAllSongs() {
+        try {
+            let response = await axios.get('http://127.0.0.1:8000/music/')
+            this.setState({
+                songs: response.data
+            })
+        }catch(ex){
+            console.log('Error in API Call!')
+        } 
+    console.log(this.state.songs)
+    }
+
+    deleteSong = (song) => {
+        // http://127.0.0.1:8000/music/3/
+        try {
+            axios.delete(`http://127.0.0.1:8000/music/${song}/`)
+        }catch (ex) {
+
+        }
+    }
+
     render() { 
         return ( 
             <div className="row">
-
+                <div className= "col-lg-3"></div>
+                <div className= "col-lg-6 table" align = "center">
+                    <h1>All Songs</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Artist</th>
+                                <th>Album</th>
+                                <th>Release Date</th>
+                            </tr>
+                        </thead>
+                        {this.state.songs.map((song) => {
+                            return <tr><td>{song.id}</td><td>{song.title}</td>
+                            <td>{song.artist}</td><td>{song.album}</td><td>{song.release_date}</td>
+                            <td><button className = "btn-danger" onClick = {this.deleteSong(song.id)}>Delete</button></td></tr>
+                        })}
+                    </table>
+                </div>
+                <div className= "col-lg-3"></div>
             </div>
          );
     }
